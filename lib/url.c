openharmony/third_party_curl

ConnectionExists: for NTLM re-use, require credentials to match
CVE-2015-3143

Bug: http://curl.haxx.se/docs/adv_20150422A.html
Reported-by: Paras Sethia

lib/url.c
@@ -3209,7 +3209,7 @@ ConnectionExists(struct SessionHandle *data,
       }
 
       if((!(needle->handler->flags & PROTOPT_CREDSPERREQUEST)) ||
-         wantNTLMhttp) {
+         (wantNTLMhttp || check->ntlm.state != NTLMSTATE_NONE)) {
         /* This protocol requires credentials per connection or is HTTP+NTLM,
            so verify that we're using the same name and password as well */
         if(!strequal(needle->user, check->user) ||

ktdreyer/curl

Upstream-commit: 31be461c6b659312100c47be6ddd5f0f569290f6
Signed-off-by: Kamil Dudka <kdudka@redhat.com>

lib/url.c
@@ -2973,7 +2973,7 @@ ConnectionExists(struct SessionHandle *data,
             }
           }
           if((!(needle->handler->flags & PROTOPT_CREDSPERREQUEST)) ||
-             ((needle->handler->protocol & CURLPROTO_HTTP) && wantNTLM)) {
+             (wantNTLM || check->ntlm.state != NTLMSTATE_NONE)) {
             /* This protocol requires credentials per connection or is HTTP+NTLM,
                so verify that we're using the same name and password as well */
             if(!strequal(needle->user, check->user) ||